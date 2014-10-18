3ef93e38eccea43988ee9f065580f7a9b3d3d7e0
Elie Andraos <elie@andraos.org>
2014-10-18 19:53:52 +0300

    api documentationsetup

--- app/views/developer/api.blade.php
+++ app/views/developer/api.blade.php
@@ -8,7 +8,19 @@
 	<div class="row">
 		<div class="col-md-12">
 			<h1>API Docs</h1>
+			<div class="row">
+				<div class="col-md-9 api-docs" data-spy="scroll" data-target="#api-navbar" data-offset="0">
+					<div class="section" id="authorization">authorization</div>
+					<div class="section" id="subscriber-create">create</div>
+					<div class="section" id="subscriber-create-profile">profile</div>
+					<div class="section" id="subscriber-add-device">device</div>
+					<div class="section" id="subscribers-list">list</div>
+					<div class="section" id="subscribers-show">show</div>
+				</div>
+				<div class="col-md-3 sidebar-nav">
+					@include('developer.partials._sidebar_api_nav')
+				</div>
+			</div>
 		</div>
 	</div>
-	<br/>
 @stop
---
+++ app/views/developer/partials/_sidebar_api_nav.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar" id="api-navbar" role="navigation" data-spy="affix" data-offset-top="150">
+	 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	 	<ul class="nav">
+	    	<li class="active"><a href="#authorization">Authorization</a></li>
+	        <li>
+	        	<a href="#">Subscribers</a>
+	        	<ul class="nav">
+	        		<li><a href="#subscriber-create">Create</a></li>
+	        		<li><a href="#subscriber-create-profile">Create profile</a></li>
+	        		<li><a href="#subscriber-add-device">Add device</a></li>
+	        		<li><a href="#subscribers-list">List</a></li>
+	        		<li><a href="#subscribers-show">Show</a></li>
+	        	</ul>
+	        </li>
+	        <li>
+	        	<a href="#">News</a>
+	        	<ul class="nav">
+	        		<li><a href="#news-list">List</a></li>
+	        		<li><a href="#news-show">Show</a></li>
+	        		<li><a href="#news-categories">Categories</a></li>
+	        		<li><a href="#news-by-category">List news by category</a></li>
+	        	</ul>
+	        <li>
+	        <li>
+	        	<a href="#news-list">Questions</a>
+	        	<ul class="nav">
+	        		<li><a href="#questions-list">List</a></li>
+	        		<li><a href="#Questions-show">Show</a></li>
+	        	</ul>
+	        <li>
+	        <li><a href="#limit-pagination">Limit and pagination</a></li>
+	        <li><a href="#http-status-codes">HTTP status codes</a></li>
+	     </ul>
+	</div>
+</nav>
