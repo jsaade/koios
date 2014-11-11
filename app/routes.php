82ad1e71ca56637131b8813c7be0b5823727367f
Elie Andraos <elie@andraos.org>
2014-11-11 17:10:10 +0200

    setup local and production env

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,5 @@
 <?php
+print App::environment();
 
 /*******************
  * Models bindings *
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,10 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('homestead'),
-
-));
+$env = $app->detectEnvironment([
+	'local'      => ['E-ANDRAOS', 'EliesMacBook-Pro.local'],
+	'production' => ['koios.mercury.me']	
+]);
 
 /*
 |--------------------------------------------------------------------------
