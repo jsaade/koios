57bf0d5cacb5086773c6b3aaffbed66df86e72d9
Elie Andraos <elie@andraos.org>
2014-12-17 01:58:33 +0200

    write the certificates to their config file

--- app/config/packages/davibennun/laravel-push-notification/config.php
+++ app/config/packages/davibennun/laravel-push-notification/config.php
@@ -1,29 +1,5 @@
 <?php 
 	return array(
 
-		'547ed69a2ebd5_IOS' => array(
-			'environment' => 'production',
-			'passPhrase' => 'laf_push@123',
-			'service' => 'apns',
-			'certificate' => 'C:\wamp\www\koios\public/uploads/547ed69a2ebd5/apns-prod-cert.pem'
-		),
-
-		'547ed69a2ebd5_ANDROID' => array(
-			'environment' => 'production',
-			'apiKey' => 'my-android-key-here',
-			'service' => 'gcm'
-		),
-
-		'54906a3bdc843_IOS' => array(
-			'environment' => 'production',
-			'passPhrase' => 'pass2',
-			'service' => 'apns',
-			'certificate' => 'C:\wamp\www\koios\public/uploads/54906a3bdc843/apns-prod-cert.pem'
-		),
-
-		'54906a3bdc843_ANDROID' => array(
-			'environment' => 'production',
-			'apiKey' => 'key-2-api',
-			'service' => 'gcm'
-		)
+	
 );
