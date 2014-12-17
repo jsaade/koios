9eaff958decd5168bf25b16b9f9db3dfe6b78bb7
Elie Andraos <elie@andraos.org>
2014-12-17 04:14:26 +0200

    commiting the file config that i am having errors on

--- app/config/packages/davibennun/laravel-push-notification/config.php
+++ app/config/packages/davibennun/laravel-push-notification/config.php
@@ -1,4 +1,16 @@
 <?php 
 	return array(
 
+		'547ed69a2ebd5_IOS' => array(
+			'environment' => 'production',
+			'passPhrase' => 'laf_push@123',
+			'service' => 'apns',
+			'certificate' => 'C:\wamp\www\koios\public/uploads/547ed69a2ebd5/apns-prod-cert.pem'
+		),
+
+		'547ed69a2ebd5_ANDROID' => array(
+			'environment' => 'production',
+			'apiKey' => 'my-android-key-here',
+			'service' => 'gcm'
+		)
 );
