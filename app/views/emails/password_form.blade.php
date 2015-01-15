a40f4e43b2d56c94be7a4e3bdb9f3ccba599063b
Elie Andraos <elie@andraos.org>
2015-01-15 14:57:12 +0200

    update password view

--- app/views/emails/password_form.blade.php
+++ app/views/emails/password_form.blade.php
@@ -1,3 +1,6 @@
+<h2>{{ $subscriber->application->name }}</h2>
+<p>Update your password:</p>
+
 {{ Form::open( ['route' => ['store.password']]) }}
 	{{ Form::hidden('code', $subscriber->verification_token ) }}
 	{{ Form::text('password', null, [ 'class' => 'form-control', 'id' => 'password'] ) }}
