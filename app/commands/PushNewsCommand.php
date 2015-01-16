63c8e7784a552bf40f901286a0bd7acba765c277
Elie Andraos <elie@andraos.org>
2015-01-16 17:04:21 +0200

    push test

--- app/commands/PushNewsCommand.php
+++ app/commands/PushNewsCommand.php
@@ -82,6 +82,7 @@ class PushNewsCommand extends Command {
 
 		$pushManager = PushNotification::PushManager('prod');
 		$appIosConfig = Config::get($application->slug.'_IOS');
+		dd($appIosConfig );
 		$apnsAdapter =  PushNotification::ApnsAdapter( ['certificate' => $appIosConfig['certificate'] ]);
 		var_dump($pushManager, $apnsAdapter);
 		die;
