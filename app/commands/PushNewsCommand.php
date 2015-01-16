38277360df4de57023900e44cdb2c49b0233faa9
Elie Andraos <elie@andraos.org>
2015-01-16 16:34:00 +0200

    test push

--- app/commands/PushNewsCommand.php
+++ app/commands/PushNewsCommand.php
@@ -88,7 +88,8 @@ class PushNewsCommand extends Command {
 			    'locArgs' => array( $n->id, $n->news_category_id)
 			));	
 			$push = $app_ios->to($devices)->send($message);
-			
+			var_dump($push);
+			die();
 			var_dump($push->getFeedback($app_ios->adapter)); 
 			
 			die('sent');
