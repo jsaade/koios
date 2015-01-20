eb877a4a370abe8b2fa4cd3e2fe8c2126c9f95fc
Elie Andraos <elie@andraos.org>
2015-01-20 10:24:59 +0200

    update composer.lock

--- app/config/workbench.php
+++ app/config/workbench.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'name' => '',
+	'name' => 'Elie Andraos',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -26,6 +26,6 @@ return array(
 	|
 	*/
 
-	'email' => '',
+	'email' => 'elie@andraos.org',
 
 );
